5829c54a3456af107245dbcbe987308e633f74ce
Wizard0f0s <jamie.gilmer@gmail.com>
2013-06-19 20:49:39 -0400

    t0lte: fix common.mk for cos vendor
    Change-Id: I258bd4f31e049bc48c45fbb042e4e2cbfce69687

--- common.mk
+++ common.mk
@@ -66,7 +66,7 @@ PRODUCT_PACKAGES += \
     Stk \
     SamsungServiceMode
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cos/config/nfc_enhanced.mk)
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
